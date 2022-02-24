f982b711aac94c1faa42a4721d4795bf5fe9c493
Neeraj Patel <neerajhpatel@gmail.com>
2022-02-24 22:36:45 +1100

    Fixed map controls container

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,19 @@
-import { Grid, Box } from '@mui/material';
-import React from 'react';
-import Map from './components/map';
-import MapControls from './components/mapControls';
-import SearchBar from './components/searchBar';
+import { Grid, Box } from "@mui/material";
+import React from "react";
+import Map from "./components/map";
+import MapControls from "./components/mapControls";
+import SearchBar from "./components/searchBar";
 
 function App() {
   return (
     <div className='App'>
-      <div className='container controls'>
-        <Grid container height={'100%'}>
-          <Grid item xs={3}>
-            <Box sx={{ display: 'grid', gap: 2, p: 2 }}>
-              <MapControls />
-              <SearchBar />
-            </Box>
-          </Grid>
-        </Grid>
+      <div className='controls-container'>
+        <Box sx={{ display: "grid", gap: 2, p: 2 }}>
+          <MapControls />
+          <SearchBar />
+        </Box>
       </div>
-      <div className='container map'>
+      <div className='map-container'>
         <Map />
       </div>
     </div>
